4cf5506b87d1981cd69933cabce393b767aa91ca
Andrés Sæmundsson <andresthor@gmail.com>
2025-03-06 23:58:30 +0100

    chore: update readme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="site-footer">
       <div className="container">
-        © {new Date().getFullYear()} Andres Saemundsson |
+        © {new Date().getFullYear()} Andres Saemundsson |{' '}
         <a href="https://github.com/andresthor">github.com/andresthor</a>
       </div>
     </footer>
